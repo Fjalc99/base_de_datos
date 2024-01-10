150170e5556f555e1b4af951ffb9538649d7c714
Fran <alcantarilla.cafra24@triana.salesianos.edu>
2024-01-10 14:30:00 +0100

    1

---
+++ Tema6/Ejercicio1.sql
@@ -0,0 +1,38 @@
+CREATE TABLE libro(
+	isbn 				VARCHAR (13), 
+	titulo				VARCHAR (80) NOT NULL,
+	dni_autor			VARCHAR (9) NOT NULL,
+	cod_genero 			INTEGER NOT NULL,
+	cod_editorial		INTEGER NOT NULL,
+	CONSTRAINT pk_isbn PRIMARY KEY (isbn)
+)
+
+CREATE TABLE autor (
+	dni					VARCHAR (9),
+	nombre				VARCHAR (100) NOT NULL,
+	nacionalidad		VARCHAR (80)
+	CONSTRAINT pk_dni PRIMARY KEY (dni)
+)
+
+CREATE TABLE editorial(
+	cod_editorial		SERIAL,
+	nombre				VARCHAR (100) NOT NULL,
+	direccion			VARCHAR (150),
+	poblacion 			VARCHAR (60),
+	CONSTRAINT pk_cod_editorial PRIMARY KEY (cod_editorial)
+)
+
+CREATE TABLE genero (
+	id_genero 			SERIAL,
+	nombre				VARCHAR (100) NOT NULL,
+	descripcion 		TEXT,
+	CONSTRAINT pk_id_genero PRIMARY KEY 
+)
+
+CREATE TABLE edicion (
+	isbn 				VARCHAR (13),
+	fecha_publicacion	DATE
+	cantidad 			INTEGER
+	CONSTRAINT cantidad_ediciones CHECK (cantidad > 0),
+	CONSTRAINT pk_edicion PRIMARY KEY (isbn, fecha_publicacion)
+)
--- Tema6/Tipos de numeros enteros,fechas etc....sql
+++ Tema6/Tipos de numeros enteros,fechas etc....sql
@@ -40,3 +40,82 @@ CREATE TABLE alumno (
 SELECT *
 FROM alumno
 
+
+--CHECK 
+CREATE TABLE producto(
+	num_producto 		INTEGER,
+	nombre		 		TEXT,
+	precio		 		NUMERIC,
+	precio_descontado 	NUMERIC,
+	CONSTRAINT precio_positivo CHECK (precio > 0),
+	CONSTRAINT precio_descontado_positivo CHECK (precio_descontado > 0),
+	CONSTRAINT descuento_valido CHECK (precio_descontado < precio)
+);
+
+INSERT INTO producto(precio)
+VALUES ('1')
+------------------------------------------------------------------------
+--PRIMARY KEY 
+drop table producto
+
+CREATE TABLE producto(
+	num_producto		INTEGER,
+	nombre				TEXT,
+	precio				NUMERIC,
+	CONSTRAINT pk_productos PRIMARY KEY (num_producto)
+);
+
+INSERT INTO producto
+VALUES ('1','Switch','299.00')
+-------------------------------------------------------------------------
+--seguir poniendolo
+CREATE TABLE pedido(
+	id_pedido INTEGER,
+	num_producto	INTEGER,
+	cantidad INTEGER,
+	CONSTRAINT  pk_pedido 
+)
+
+------------------------------------------------------------------------------
+--El tipo de dato de una clave primaria si es serial debemos poner un de tipo integer que corresponda
+--Y el tipo de ser igual siempre que no sea serial
+DROP TABLE alumno
+
+CREATE TABLE alumno(
+	id_alumno SERIAL,
+	nommbre	TEXT,
+	CONSTRAINT pk_alumno PRIMARY KEY (id_alumno)
+);
+
+CREATE TABLE asignatura(
+	id_asignatura SERIAL,
+	nombre VARCHAR (100),
+	profesor VARCHAR (200),
+	CONSTRAINT pk_asignatura PRIMARY KEY (id_asignatura)
+);
+
+CREATE TABLE matricula(
+	id_alumno INTEGER,
+	id_asignatura INTEGER,
+	anio_escolar VARCHAR(10),
+	CONSTRAINT pk_matricula PRIMARY KEY (id_alumno, id_asignatura, anio_escolar),
+	CONSTRAINT fk_matricula_alumno FOREIGN KEY (id_alumno) REFERENCES alumno,
+	CONSTRAINT fk_matricula_asignatura FOREIGN KEY (id_asignatura) REFERENCES asignatura
+	
+);
+
+CREATE TABLE nota (
+	id_alumno INTEGER,
+	id_asignatura INTEGER,
+	anio_escolar VARCHAR (10),
+	tipo_evaluacion VARCHAR (1),
+	nota NUMERIC(4,2),
+	CONSTRAINT pk_nota PRIMARY KEY
+	(id_alumno, id_asignatura, anio_escolar,
+	tipo_evaluacion),
+	CONSTRAINT ck_nota_tipo_evaluacion
+	CHECK (tipo_evaluacion IN ('1','2','3','F')),
+	CONSTRAINT fk_nota_matricula FOREIGN KEY (id_alumno, id_asignatura, anio_escolar) 
+	REFERENCES matricula (id_alumno, id_asignatura, anio_escolar)
+	
+);
