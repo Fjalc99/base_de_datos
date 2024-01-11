49e49dec52ced679c99e41474388e96b57fdae4b
Francisco José Alcantarilla Calado <alcantarilla.cafra24@triana.salesianos.edu>
2024-01-11 18:42:55 +0100

    Ejercios2

---
+++ Tema6/EjerciciosTema6/Ejercicio2 ALTER TABLE.sql
@@ -0,0 +1,50 @@
+DROP TABLE IF EXISTS libro CASCADE
+CREATE TABLE libro(
+	isbn 				VARCHAR (13), 
+	titulo				VARCHAR (80) NOT NULL,
+	dni_autor			VARCHAR (9) NOT NULL,
+	cod_genero 			INTEGER NOT NULL,
+	cod_editorial		INTEGER NOT NULL,
+	CONSTRAINT pk_isbn PRIMARY KEY (isbn)	
+)
+
+DROP TABLE IF EXISTS autor CASCADE
+CREATE TABLE autor (
+	dni					VARCHAR (9),
+	nombre				VARCHAR (100) NOT NULL,
+	nacionalidad		VARCHAR (80),
+	CONSTRAINT pk_dni PRIMARY KEY (dni)
+)
+
+DROP TABLE IF EXISTS editorial CASCADE
+CREATE TABLE editorial(
+	cod_editorial		SERIAL,
+	nombre				VARCHAR (100) NOT NULL,
+	direccion			VARCHAR (150),
+	poblacion 			VARCHAR (60),
+	CONSTRAINT pk_cod_editorial PRIMARY KEY (cod_editorial)
+)
+
+DROP TABLE IF EXISTS genero CASCADE
+CREATE TABLE genero (
+	id_genero 			SERIAL,
+	nombre				VARCHAR (100) NOT NULL,
+	descripcion 		TEXT,
+	CONSTRAINT pk_id_genero PRIMARY KEY (id_genero)
+)
+
+DROP TABLE IF EXISTS edicion CASCADE
+CREATE TABLE edicion (
+	isbn 				VARCHAR (13),
+	fecha_publicacion	DATE,
+	cantidad 			INTEGER,
+	CONSTRAINT cantidad_ediciones CHECK (cantidad > 0),
+	CONSTRAINT pk_edicion PRIMARY KEY (isbn, fecha_publicacion)
+
+)
+
+
+ALTER TABLE libro ADD FOREIGN KEY (dni_autor) REFERENCES autor (dni)
+ALTER TABLE libro ADD FOREIGN KEY (cod_genero) REFERENCES genero (id_genero)
+ALTER TABLE libro ADD FOREIGN KEY (cod_editorial) REFERENCES editorial (cod_editorial)
+ALTER TABLE edicion ADD FOREIGN KEY (isbn) REFERENCES libro (isbn)
---
+++ Tema6/EjerciciosTema6/Ejercicio2 FOREIGN KEY.sql
@@ -0,0 +1,47 @@
+DROP TABLE IF EXISTS libro CASCADE
+CREATE TABLE libro(
+	isbn 				VARCHAR (13), 
+	titulo				VARCHAR (80) NOT NULL,
+	dni_autor			VARCHAR (9) NOT NULL,
+	cod_genero 			INTEGER NOT NULL,
+	cod_editorial		INTEGER NOT NULL,
+	CONSTRAINT pk_isbn PRIMARY KEY (isbn),
+	CONSTRAINT fk_dni_autor FOREIGN KEY (dni_autor) REFERENCES autor (dni), 
+	CONSTRAINT fk_cod_genero FOREIGN KEY (cod_genero) REFERENCES genero (id_genero),
+	CONSTRAINT fk_cod_editorial FOREIGN KEY (cod_editorial) REFERENCES editorial (cod_editorial)
+)
+
+DROP TABLE IF EXISTS autor CASCADE
+CREATE TABLE autor (
+	dni					VARCHAR (9),
+	nombre				VARCHAR (100) NOT NULL,
+	nacionalidad		VARCHAR (80),
+	CONSTRAINT pk_dni PRIMARY KEY (dni)
+)
+
+DROP TABLE IF EXISTS editorial CASCADE
+CREATE TABLE editorial(
+	cod_editorial		SERIAL,
+	nombre				VARCHAR (100) NOT NULL,
+	direccion			VARCHAR (150),
+	poblacion 			VARCHAR (60),
+	CONSTRAINT pk_cod_editorial PRIMARY KEY (cod_editorial)
+)
+
+DROP TABLE IF EXISTS genero CASCADE
+CREATE TABLE genero (
+	id_genero 			SERIAL,
+	nombre				VARCHAR (100) NOT NULL,
+	descripcion 		TEXT,
+	CONSTRAINT pk_id_genero PRIMARY KEY (id_genero)
+)
+
+DROP TABLE IF EXISTS edicion CASCADE
+CREATE TABLE edicion (
+	isbn 				VARCHAR (13),
+	fecha_publicacion	DATE,
+	cantidad 			INTEGER,
+	CONSTRAINT cantidad_ediciones CHECK (cantidad > 0),
+	CONSTRAINT pk_edicion PRIMARY KEY (isbn, fecha_publicacion),
+	CONSTRAINT fk_isbn_edicion FOREIGN KEY (isbn) REFERENCES libro 
+)
